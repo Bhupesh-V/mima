62a65a2664435c64f9ac37a9a7cc58287ce7ff24
Bhupesh-V <varshneybhupesh@gmail.com>
2021-03-10 22:29:08 +0530

    move back to .env

--- store.py
+++ store.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 
-env_path = Path(".") / ".flaskenv"
+env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
